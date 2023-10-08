17bbe4ad61fb2c2b650f572e7ac5dbeebdc6a345
Jose Ramirez <jramireztolentino@gmail.com>
2023-10-08 12:15:41 -0400

    End of section 7th

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,7 +11,10 @@ export class GifsService {
   private apikey:     string = '7rNrvhavLkoYXU4ockLFvqaqNYjPhTeA';
   private serviceUrl: string = 'http://api.giphy.com/v1/gifs';
 
-  constructor( private http: HttpClient ) { }
+  constructor( private http: HttpClient ) {
+    this.loadLocalStorage();
+    console.log('Gifs Service Ready');
+   }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -21,11 +24,25 @@ export class GifsService {
     tag = tag.toLowerCase();
 
     if(this._tagsHistory.includes( tag )) {
-      this._tagsHistory = this._tagsHistory.filter( (oldTag) => { oldTag !== tag } )
+      this._tagsHistory = this._tagsHistory.filter( (oldTag) =>  oldTag !== tag  )
     }
 
     this._tagsHistory.unshift( tag );
-    this._tagsHistory = this.tagsHistory.splice(0, 15);
+    this._tagsHistory = this._tagsHistory.splice(0,15);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage():void {
+    localStorage.setItem( 'history', JSON.stringify( this._tagsHistory ) );
+  }
+
+  private loadLocalStorage():void {
+    if ( !localStorage.getItem( 'history' ) ) return;
+
+    this._tagsHistory = JSON.parse( localStorage.getItem( 'history' )! );
+
+    if ( this._tagsHistory.length === 0 ) return;
+    this.searchTag( this._tagsHistory[0] );
   }
 
   searchTag( tag: string ):void {
@@ -34,7 +51,7 @@ export class GifsService {
 
     const params = new HttpParams()
       .set('api_key', this.apikey)
-      .set('limit', '10')
+      .set('limit', '16')
       .set('q', tag)
 
     this.http.get<SearchResponse>(`${ this.serviceUrl }/search`, { params })
